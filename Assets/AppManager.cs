f68cd9976e0d761bc1185c80838f576f9de55d97
Ekatherina Chernikova <59030208+pepe-Di@users.noreply.github.com>
2021-10-18 18:05:44 +0700

    .

--- Assets/AppManager.cs
+++ Assets/AppManager.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 using System;
 public class AppManager : MonoBehaviour
 {
+    public GameObject panel;
+    public Button button;
+    public GameObject Prefab;
     public Dropdown dropdown;
+    public GameObject Content;
     public InputField SumField, TermField, PerField, DateField;
     public DateTime date;
     public float sum, term, per, pay;
@@ -13,18 +17,34 @@ public class AppManager : MonoBehaviour
     public Text PayTxt, OverTxt, TotalTxt;
     void Awake()
     {
+        button.enabled = false;
         SumField = GameObject.Find("SumField").GetComponent<InputField>();
         TermField = GameObject.Find("TermField").GetComponent<InputField>();
         PerField = GameObject.Find("PerField").GetComponent<InputField>();
         dropdown = GameObject.Find("Dropdown").GetComponent<Dropdown>();
     }
+    private void Update()
+    {
+        if (SumField.text != null&& TermField.text != null&& PerField.text != null) button.enabled = true;
+    }
     public void OkButtonClick() 
     {
-       // try
-      //  {
-            sum = float.Parse(SumField.text);
-            term = float.Parse(TermField.text);
-            per = float.Parse(PerField.text);
+        try
+        {
+            try
+            {
+                sum = float.Parse(SumField.text);
+                term = float.Parse(TermField.text);
+                per = float.Parse(PerField.text);
+            }
+            catch 
+            {
+                SumField.text = "";
+                TermField.text = ""; 
+                PerField.text = "";
+                return;
+            }
+            panel.SetActive(true);
             if (dropdown.value == 0) 
             {
                 term *= 12;
@@ -32,36 +52,45 @@ public class AppManager : MonoBehaviour
             per /= 100;
             pay = sum / term;
             float overpay = 0;
-               for (float i = 0; i < term; i++)
-               {
+            for (float i = 0; i < term; i++)
+            {
                 Payment payment = new Payment();
-                payment.Left = sum - pay * i;
-                payment.Pay = (sum - pay * i) * per / 12;
-                payment.Total = pay + (sum - pay * i) * per / 12;
+                payment.values[0] = i + 1;
+                payment.values[1] = sum - pay * i;
+                payment.values[2] = pay;
+                payment.values[3] = (sum - pay * i) * per / 12;
+                payment.values[4] = pay + (sum - pay * i) * per / 12;
                    
-                   overpay += payment.Total;
-                   payments.Add(payment);
-               }
-               foreach(Payment payment in payments)
-               {
-               }
+                overpay += payment.values[4];
+                payments.Add(payment);
+            }
+            foreach(Payment payment in payments)
+            {
+                 for (int i = 0; i<5; i++) 
+                 {
+                    GameObject gm = Instantiate(Prefab, new Vector3(0, 0, 0), Quaternion.identity);
+                    gm.transform.SetParent(Content.transform);
+                    gm.transform.localScale = new Vector3(1, 1, 1);
+                    Text txt = gm.GetComponent<Text>();
+                    txt.text = payment.values[i].ToString();
+                }
+            }
             Debug.Log(pay.ToString() + " ₽");
             PayTxt.text = pay.ToString() + " ₽";
-               TotalTxt.text = overpay.ToString() + " ₽";
-                overpay -= sum;
-                OverTxt.text = overpay.ToString() + " ₽";
-     //   }
-      //  catch 
-      //  {
-      //      Debug.Log("error");
-       // }
+            TotalTxt.text = overpay.ToString() + " ₽";
+            overpay -= sum;
+            OverTxt.text = overpay.ToString() + " ₽";
+        }
+        catch 
+        {
+        }
     }
 }
-
 public class Payment
 {
-    public float Left { get; set; }
-    public float Pay { get; set; }
-    public float Total { get; set; }
-    public Payment() { }
+    public float[] values;
+    public Payment() 
+    {
+        values = new float[5];
+    }
 }
